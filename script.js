5dea13024e3edb97bc33af8531275fa7c25429a8
KaweiCZ <101064567+KaweiCZ@users.noreply.github.com>
2022-04-22 20:42:23 +0200

    Ver. 3.3

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const footer = document.querySelector(".footer");
 const date = new Date();
+const menuButton = document.querySelector(".nav__menu-button");
+const menu = document.querySelector(".nav__list");
 
 footer.innerHTML += " " + date.getFullYear();
+
+menuButton.addEventListener("click", function () {
+  menu.classList.toggle("nav__list--hidden");
+});
